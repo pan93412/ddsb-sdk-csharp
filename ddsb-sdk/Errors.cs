45de498606edc280064e31d55cf40ae615be861d
pan93412 <pan93412@gmail.com>
2020-07-08 14:58:09 +0800

    docs(sdk/errors): add a sumary for errors

--- ddsb-sdk/Errors.cs
+++ ddsb-sdk/Errors.cs
@@ -4,10 +4,25 @@ namespace ddsb_sdk
 {
     public static class Errors
     {
+        /// <summary>
+        /// 10000: The given url is empty.
+        /// </summary>
         public static Exception err10000 = new InvalidOperationException("The given url is empty.");
+        /// <summary>
+        /// 10001: The given url is malformed.
+        /// </summary>
         public static Exception err10001 = new ArgumentException("The given url is malformed. Add the \"http://\" or \"https://\" prefix?");
+        /// <summary>
+        /// 10002: The given suffix is empty.
+        /// </summary>
         public static Exception err10002 = new ArgumentException("The given suffix is empty.");
+        /// <summary>
+        /// 10003: The given password is empty.
+        /// </summary>
         public static Exception err10003 = new ArgumentException("The given password is empty.");
+        /// <summary>
+        /// 10004: The given suffix has been used.
+        /// </summary>
         public static Exception err10004 = new InvalidOperationException("The given suffix has been used.");
     }
 }
